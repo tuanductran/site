9ec638ebac9ef3b74ee83fb096a8347c6c95865f
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-09-14 21:44:31 +0700

    Update filterTags.ts

--- src/lib/filterTags.ts
+++ src/lib/filterTags.ts
@@ -1,14 +1,9 @@
 export function filterTags(dataList: any[], tagFilter: string) {
-  // Sort the list by the createdAt property, from newest to oldest
-  const sortedData = dataList.sort((a, b) => {
-    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  })
-
   // If no tag is provided, return the sorted list
   if (!tagFilter) {
-    return sortedData
+    return dataList
   }
 
   // Filter the list to return only items that include the specified tag
-  return sortedData.filter(item => item.tags && item.tags.includes(tagFilter))
+  return dataList.filter(item => item.tags && item.tags.includes(tagFilter))
 }
