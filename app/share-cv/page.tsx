0c6e675673bbe16f5aa344f61605945d7a69319f
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-09-26 23:34:02 +0700

    Fix error components

--- app/share-cv/page.tsx
+++ app/share-cv/page.tsx
@@ -1,5 +1,5 @@
 import CVForm from '@components/cvForm'
-import { PageLayout } from '@components/PageContainer'
+import { PageContainer } from '@components/PageContainer'
 
 const seoTitle = 'Share Your CV'
 const seoDescription = 'Easily and quickly share your CV to stand out to potential employers.'
@@ -11,8 +11,8 @@ export const metadata = {
 
 export default function ShareCV() {
   return (
-    <PageLayout title={seoTitle} intro={seoDescription}>
+    <PageContainer title={seoTitle} intro={seoDescription}>
       <CVForm />
-    </PageLayout>
+    </PageContainer>
   )
 }
