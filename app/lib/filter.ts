5801a3d621d84a5e4b84fc9dda5fabce9ca29167
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-09-28 23:01:04 +0700

    Update filter.ts

--- app/lib/filter.ts
+++ app/lib/filter.ts
@@ -1,21 +1,12 @@
-export function filterStatus(type: any, status: any) {
-  const sortedStatus = [...type].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-
-  if (!status) {
-    return sortedStatus
-  }
-
-  return sortedStatus.filter(type => type.status.string.includes(status))
-}
-
-export function filterTags(type: any, tag: any) {
-  const sortedTags = [...type].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-
-  if (!tag) {
-    return sortedTags
-  }
-
-  return sortedTags.filter(type => type.tags.includes(tag))
+export function filterStatus(articles, selectedTag) {
+  return articles
+    .sort((a, b) => Number(new Date(b.createdAt)))
+    .filter(article => {
+      if (selectedTag === null) {
+        return true
+      }
+      return article.status.string.includes(selectedTag)
+    })
 }
 
 export function filterArticles(articles, selectedTag) {
