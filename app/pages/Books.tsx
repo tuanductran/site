6cf273260b4875a0e6c4f9b46e4c68e00e0c27bc
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-03 21:08:59 +0700

    Fix error

--- app/pages/Books.tsx
+++ app/pages/Books.tsx
@@ -23,26 +23,29 @@ export function Books({ title, description, books, status }: Props) {
     <PageContainer title={title} intro={description}>
       <NotionStatus status={status} selectedStatus={selectedStatus} setSelectedStatus={setSelectedStatus} />
       <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {filteredBooks.map((book: NotionBooks) => (
-          <Link href={book.slug} key={book.id} className="group relative flex flex-col rounded-xl border border-zinc-200/50 bg-white p-6 shadow-xl shadow-zinc-300/10 dark:shadow-none dark:border-zinc-800/80 dark:bg-gradient-to-bl dark:bg-zinc-900 dark:from-zinc-950/5 dark:to-zinc-950/30 hover:bg-zinc-50 dark:hover:bg-zinc-800/50" title={book.title}>
-            <div className="flex items-center space-x-3.5">
-              <h2 className="text-base text-zinc-800 dark:text-zinc-100">
-                {book.title}
-              </h2>
-              {book.status && (
-                <span className="inline-flex items-center leading-none rounded-full bg-sky-50/10 dark:bg-light/5 px-2 py-1 text-xs font-medium text-dark dark:text-light ring-1 ring-inset ring-primary/20 dark:ring-light/20">
-                  {book.status}
-                </span>
-              )}
-            </div>
-            <p className="flex-1 mt-4 mb-1 text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2">{book.description}</p>
-            <span className="flex items-center space-x-1 mt-4 rounded-full dark:hover:bg-transparent text-zinc-800 dark:text-white group-hover:text-primary dark:group-hover:text-light font-medium w-fit py-1 text-sm">
-              View book
-              {' '}
-              <ChevronRightIcon className="ml-1 h-auto w-4 stroke-current" />
-            </span>
-          </Link>
-        ))}
+        {filteredBooks.map((book: NotionBooks) => {
+          const booksStatus = book.status.string
+          return (
+            <Link href={book.slug} key={book.id} className="group relative flex flex-col rounded-xl border border-zinc-200/50 bg-white p-6 shadow-xl shadow-zinc-300/10 dark:shadow-none dark:border-zinc-800/80 dark:bg-gradient-to-bl dark:bg-zinc-900 dark:from-zinc-950/5 dark:to-zinc-950/30 hover:bg-zinc-50 dark:hover:bg-zinc-800/50" title={book.title}>
+              <div className="flex items-center space-x-3.5">
+                <h2 className="text-base text-zinc-800 dark:text-zinc-100">
+                  {book.title}
+                </h2>
+                {booksStatus && (
+                  <span className="inline-flex items-center leading-none rounded-full bg-sky-50/10 dark:bg-light/5 px-2 py-1 text-xs font-medium text-dark dark:text-light ring-1 ring-inset ring-primary/20 dark:ring-light/20">
+                    {booksStatus}
+                  </span>
+                )}
+              </div>
+              <p className="flex-1 mt-4 mb-1 text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2">{book.description}</p>
+              <span className="flex items-center space-x-1 mt-4 rounded-full dark:hover:bg-transparent text-zinc-800 dark:text-white group-hover:text-primary dark:group-hover:text-light font-medium w-fit py-1 text-sm">
+                View book
+                {' '}
+                <ChevronRightIcon className="ml-1 h-auto w-4 stroke-current" />
+              </span>
+            </Link>
+          )
+        })}
       </ul>
     </PageContainer>
   )
