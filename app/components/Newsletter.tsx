1733fbe4086d08b10550075d4452563f078dc305
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-03 20:58:54 +0700

    Update UI

--- app/components/Newsletter.tsx
+++ app/components/Newsletter.tsx
@@ -22,7 +22,7 @@ export function Newsletter() {
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-primary focus:outline-none focus:ring-4 focus:ring-primary/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-light dark:focus:ring-light/10 sm:text-sm"
         />
-        <Button className="ml-4 flex-none" href="/create-cv">
+        <Button className="ml-4 flex-none" href="/create-cv" title="Get Started">
           Get Started
         </Button>
       </div>
