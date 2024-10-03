32d4fcb44052a25e5a4a85b0e9004e41f46e83a0
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-03 19:36:14 +0700

    aa

--- app/error.tsx
+++ app/error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
       <main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
         <div className="py-16">
           <div className="text-center">
-            <p className="text-base font-semibold text-primary capitalize">Error!</p>
+            <p className="text-base font-semibold text-primary">Error!</p>
             <PageTitle>Something went wrong.</PageTitle>
             <p className="mt-2 text-base text-gray-500">Oh no, something went wrong... maybe refresh?</p>
           </div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
         <div className="py-16">
           <div className="text-center">
-            <p className="text-base font-semibold text-primary capitalize">Not found!</p>
+            <p className="text-base font-semibold text-primary">Not found!</p>
             <PageTitle>Oh no! This page doesn't exist.</PageTitle>
             <p className="mt-2 text-base text-gray-500">
               If you expected to see something here, let me know (tuanductran.dev@gmail.com).
