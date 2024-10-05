68274bf98a3f47fb5404d6da8e384ee97fe9b045
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-05 10:14:30 +0700

    Update incr.ts
    Signed-off-by: Tuan Duc Tran <tuanductran.dev@gmail.com>

--- pages/incr.ts
+++ pages/incr.ts
@@ -4,10 +4,6 @@ import { NextResponse } from 'next/server'
 
 const redis = Redis.fromEnv()
 
-export const config = {
-  runtime: 'experimental-edge',
-}
-
 export default async function incr(req: NextRequest): Promise<NextResponse> {
   if (req.method !== 'POST') {
     return new NextResponse('use POST', { status: 405 })
