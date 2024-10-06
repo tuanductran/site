aa4102939192d3aebaf5349dcce2ca040a0fdc55
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-06 12:07:41 +0700

    Create environment.d.ts

---
+++ environment.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable ts/no-unused-vars */
+/* eslint-disable unused-imports/no-unused-vars */
+namespace NodeJS {
+  interface ProcessEnv {
+    NOTION_DATABASE_ID: string
+    ARTICLES_DATABASE_ID: string
+    BOOKS_DATABASE_ID: string
+    CV_NOTION_TOKEN: string
+    CV_NOTION_DATABASE_ID: string
+    NOTION_TOKEN: string
+  }
+}
