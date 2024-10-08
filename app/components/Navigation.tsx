b46048d1f080ae40066b7a67dec173d86c003103
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-08 15:31:45 +0700

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -53,7 +53,7 @@ export function MobileNavItem({ children, href }: PropsWithChildren<{ href: stri
 export function DesktopNavigation(props: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 whitespace-nowrap">
         {siteConfig.navItems.map(item => (
           <NavItem key={item.href} href={item.href}>
             {item.name}
